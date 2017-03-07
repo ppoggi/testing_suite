37b450b20b47b2384ddfb6b75453222b3b2120d6
ppoggi <ppoggi92@gmail.com>
2017-03-07 17:19:32 -0500

    Commit_2: Reverted changes and added more tests
    After reverting, added more tests.

--- src/testing_suite/EpicTests.java
+++ src/testing_suite/EpicTests.java
@@ -15,7 +15,7 @@ public class EpicTests {
 	
 		SuccessMessage successMessage = new SuccessMessage();
 		
-		assertTrue("The return form the 'SuccessMessage' class is within range of the expected results.", successMessage.data() == "Success.");
+		assertTrue("The return form the 'SuccessMessage' class is not within range of the expected results.", successMessage.data() == "Success.");
 	}
 
 	@Test	
@@ -23,7 +23,7 @@ public class EpicTests {
 	
 		ErrorMessage errorMessage = new ErrorMessage();
 		
-		assertTrue("The return form the 'SuccessMessage' class is within range of the expected results.", errorMessage.data() == "Error.");
+		assertTrue("The return form the 'SuccessMessage' class is not within range of the expected results.", errorMessage.data() == "Error.");
 	}
 	
 	@Test
@@ -33,8 +33,18 @@ public class EpicTests {
 		
 		Message errorMessage = messageHandler.generateMessageObj(0);
 		
-		assertTrue("The class 'MessageHandler' function generateMessageObj does not return an appropriate error message.", errorMessage.data() == "Error.");	
+		assertTrue("The class 'MessageHandler' function 'generateMessageObj' does not return an appropriate error message.", errorMessage.data() == "Error.");	
 	}
+	
+	@Test
+	public void class_DataMessage_function_data_generates_expected_result(){
+		
+		DataMessage dataMessage = new DataMessage();
+		
+		assertTrue("The class 'DataMessage' function 'data does not return an appropriate error message.", dataMessage.data() == "{status: 'OK', Data: 'Lorem Ipsum Dolor' }");
+		
+	}
+	
 	
 	@Test
 	public void class_MessageHandler_function_generateMessageObj_generates_expected_result(){
@@ -46,11 +56,27 @@ public class EpicTests {
 		assertTrue("The class 'MessageHandler' function generateMessageObj does not return an appropriate expected result.", successMessage.data() == "Success.");
 	}
 	
-	
-	//test out how mocking works
+	@Test
+	public void class_EpicData_function_retrieveData_generates_expected_result(){
+		
+		EpicData epicData = new EpicData();
+		
+		Message dataMessage = epicData.retriveData(1);
+
+		assertTrue("The class 'EpicData' function 'retrieve' does not return an appropriate expected result.", dataMessage.data() == "{status: 'OK', Data: 'Lorem Ipsum Dolor' }");
+		
+		
+	}
+
 	@Test
 	public void class_EpicService_function_getUserDataWithId_returns_valid_response(){
+	
+		EpicData epicData = new EpicData();
+		EpicService epicService = new EpicService(epicData);
 		
+		Message serviceMessage =  epicService.getUserDataWithId(1);
+		
+		assertTrue("The class 'EpicService function 'getUserDataWithId' does not return an appropriate expected result.", serviceMessage.data() == "{status: 'OK', Data: 'Lorem Ipsum Dolor' }");
 	}
 	
 	
